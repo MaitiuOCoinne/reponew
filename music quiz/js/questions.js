00dbc3468c51ece7ae05812185fc4c2aee03db3f
MaitiuOCoinne <105300885+MaitiuOCoinne@users.noreply.github.com>
2022-05-11 23:01:16 +0100

    Add files via upload

---
+++ music quiz/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What was Fontaines DC's debut album?",
+    answer: "Dogrel",
+    options: [
+      "Dogrel",
+      "A Hero's Death",
+      "Skinty  Fia",
+      "Couldn't tell ye mate"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which Stanglers album contained the single: Golden Brown",
+    answer: "La Folie",
+    options: [
+      "No more Heroes",
+      "La Folie",
+      "Black and White",
+      "Dreamtime"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who is the frontman of the strokes",
+    answer: "Julian Casablancas",
+    options: [
+      "Joe McDonnel",
+      "Patsy V",
+      "Julian Casablancas",
+      "Ted Sharkey"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What year was Bobby Womack's Across 110th Street released",
+    answer: "1973",
+    options: [
+      "1916",
+      "1981",
+      "1962",
+      "1973"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Bicep are from what Irish city?",
+    answer: "Belfast",
+    options: [
+      "Derry",
+      "Belfast",
+      "Dublin",
+      "Cork"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
